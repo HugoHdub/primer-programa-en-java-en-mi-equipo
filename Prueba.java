db7c02e43c687ff1bdea3ccda0b3ade2bec843aa
Usuario <usuario@gmail.com>
2024-09-25 13:31:54 +0200

    Prueba.java

--- Prueba.java
+++ Prueba.java
@@ -2,9 +2,9 @@ public class Prueba {
 
 	public static void main(String args[]) {
 
-		System.out.println("Cordero Pelayo");
 		System.out.println("18 y/o - 2006");
 		System.out.println("Lasaña");
+		System.out.println("Ken Carson");
 		
 
 	}
